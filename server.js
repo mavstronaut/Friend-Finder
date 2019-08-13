0ee78885d4bd2eb80eb2e29a48ecc3be1cd1ed1b
mavstronaut <mavbarona@gmail.com>
2019-08-13 10:05:44 -0400

    requiring server paths in server.js

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT;
 
+app.use(express.static(path.join(__dirname, './app/public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true} ));
+app.use(bodyParser.text());
+
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function() {
+    console.log('Friend Finder is listening on PORT: ' + PORT);
+});
